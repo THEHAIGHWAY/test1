c4162048287211b888e8f9becc4f206d2fac8ec0
emmanuelkuidjo <55919350+emmanuelkuidjo@users.noreply.github.com>
2023-06-29 04:28:25 +0200

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "THE HAIGHWAY"
+copyright = "2023, THE HAIGHWAY or its affiliates. All rights reserved."
+author = "Emmanuel KUIDJO"
+
+# The full version, including alpha/beta/rc tags
+release = "1.0.0"
